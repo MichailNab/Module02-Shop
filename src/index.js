ac2da8242912cc2901f24fd63415a4ff18d7b8ba
MichailNab <mishann82@gmail.com>
2022-12-23 14:43:24 +0300

    add files

--- src/index.js
+++ src/index.js
@@ -21,6 +21,20 @@ var swiper = new Swiper(".offerSwiper", {
 		prevEl: ".swiper-button-prev",
 	},
 	modules: [Navigation, Pagination],
+	breakpoints: {
+		320: {
+			slidesPerView: 1,
+			spaceBetween: 1
+		},
+		768: {
+			slidesPerView: 2,
+			spaceBetween: 2
+		},
+		1000: {
+			slidesPerView: 3,
+			spaceBetween: 3
+		}
+	}
 });
 
 var swiper = new Swiper(".trendsSwiper", {
